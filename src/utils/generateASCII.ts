04f9339fd09366d63978b94f24b5bb80c1b74f3e
WaYyTempest <contact@wayy.fr>
2025-09-08 13:58:01 +0200

    Fix error with font

--- src/utils/generateASCII.ts
+++ src/utils/generateASCII.ts
@@ -1,9 +1,9 @@
-import figlet, { Fonts } from "figlet";
+import figlet from "figlet";
 
 // Generate ASCII art from a string using the specified font
 export function generateASCII(
   text: string,
-  font: Fonts = "ANSI Shadow"
+  font: string = "ANSI Shadow"
 ): string {
   return figlet.textSync(text, { font });
 }
